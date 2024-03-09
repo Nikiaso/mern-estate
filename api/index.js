a5fe6b59ca48cd72b41b41ad4ff2d1a7988b19a6
YASHU KUMAR <nikiaso.yashu@gmail.com>
2024-03-09 17:14:43 +0530

    created a test api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import colors from "colors";
 import { connectDB } from "./config/db.js";
+import userRouter from "./routes/user.route.js";
 connectDB();
 const app = express();
+app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
   console.log(`Server is listening on port 3000...`.yellow.bold);
