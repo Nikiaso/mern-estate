e3e0c9ae1326c4a656cb27dcafd4dd80a1e45e2b
YASHU KUMAR <nikiaso.yashu@gmail.com>
2024-03-09 17:54:28 +0530

    created signup-api

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import colors from "colors";
 import { connectDB } from "./config/db.js";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 connectDB();
 const app = express();
+app.use(express.json());
 app.use("/api/user", userRouter);
-
+app.use("/api/auth", authRouter);
 app.listen(3000, () => {
   console.log(`Server is listening on port 3000...`.yellow.bold);
 });
